a1689231e38082a24920b237359c0f138ad448bd
Reinout Roels <reinout.roels@gmail.com>
2021-08-08 21:38:01 +0200

    Add timemanager

---
+++ src/TimeManager.h
@@ -0,0 +1,28 @@
+
+#ifndef WATCHY_TIMEMANAGER_H
+#define WATCHY_TIMEMANAGER_H
+
+#include <TimeLib.h>
+#include <Arduino.h>
+#include <DS3232RTC.h>
+
+class TimeManager {
+
+private:
+    time_t compile_time;
+    tmElements_t currentTime;
+    char time_str_buff[32];
+    DS3232RTC RTC;
+
+private:
+    void parseCompileTime();
+
+public:
+    TimeManager();
+    time_t getTime();
+    String getTimeAsString(const char* format);
+
+};
+
+
+#endif //WATCHY_TIMEMANAGER_H
