f7254414b0c082bb1888c70b77f4497e672a1511
RoelandMatthijssens <fulgens.ailurus@gmail.com>
2021-08-08 21:54:20 +0200

    Add time to watchface

--- src/watchface.cpp
+++ src/watchface.cpp
@@ -4,12 +4,19 @@ GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(WATCHY_
 
 const int FOREGROUND = DARK_MODE ? GxEPD_WHITE : GxEPD_BLACK;
 const int BACKGROUND = DARK_MODE ? GxEPD_BLACK : GxEPD_WHITE;
+TimeManager tm = TimeManager();
 
 void text_at(String s, int x, int y){
   display.setFont(&FreeMonoBold9pt7b);
   display.setCursor(x, y);
   display.setTextColor(FOREGROUND);
   display.println(s);
+}
+
+void time_at(int x, int y){
+  String time_string = tm.getTimeAsString("%H:%M:%S");
+  Serial.println(time_string);
+  text_at(time_string, x, y);
 }
 
 void refresh(){
@@ -21,10 +28,13 @@ void refresh(){
 void full_update_watch_face(){
   refresh();
   text_at("Meep Meep", 0, 30);
+  time_at(100, 100);
   display.display(false);
 }
 
 
 void partial_update_watch_face(){
   Serial.println("doing partial watchface update");
+  time_at(100, 100);
+  display.display(true);
 }
--- src/watchface.h
+++ src/watchface.h
@@ -2,6 +2,7 @@
 #include <GxEPD2_BW.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include "config.h"
+#include "TimeManager.h"
 
 void full_update_watch_face();
 void partial_update_watch_face();
