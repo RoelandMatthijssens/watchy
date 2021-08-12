cb923042102a852938b9f49df4075fc52f0a48e7
RoelandMatthijssens <fulgens.ailurus@gmail.com>
2021-08-12 22:24:56 +0200

    Add partial rendering of time

--- src/tasks/render.h
+++ src/tasks/render.h
@@ -11,15 +11,30 @@ GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(WATCHY_
 
 void render( void *context_vp ){
   Context *context = (Context *) context_vp;
-  display.init();
+  display.init(0, true);
   display.setFullWindow();
+  display.display(false);
   display.setTextColor(FOREGROUND);
   while(1){
-    display.fillScreen(BACKGROUND);
-    display.setCursor(50, 50);
-    display.println(context->watchface->current_time);
-    display.display(true);
-    vTaskDelay( 60000 / portTICK_PERIOD_MS );
+    int16_t  x_off, y_off;
+    uint16_t w, h;
+    String current_time = context->watchface->current_time;
+
+    display.getTextBounds(current_time, 0, 0, &x_off, &y_off, &w, &h);
+    int16_t x_pos = (DISPLAY_WIDTH-w) / 2;
+    int16_t y_pos = (DISPLAY_HEIGHT-h) / 2;
+    display.setPartialWindow(x_pos+x_off, y_pos+y_off, w, h);
+
+    display.firstPage();
+    do
+    {
+        display.fillScreen(BACKGROUND);
+        display.setCursor(x_pos, y_pos);
+        display.print(current_time);
+    }
+    while (display.nextPage());
+
+    vTaskDelay( 500 / portTICK_PERIOD_MS );
   }
 }
 
